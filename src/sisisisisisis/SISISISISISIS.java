20343c7b5b555eb788210818f0ac1036156a05f0
naxxio <77938589+naxxio@users.noreply.github.com>
2021-03-04 11:58:46 +0100

    Actualizado

---
+++ src/sisisisisisis/SISISISISISIS.java
@@ -0,0 +1,39 @@
+/**
+ * CLase que me chupa el pito
+ * @author Ignacio
+ * @version 1.1.1.1 iufb
+ */
+package sisisisisisis;
+
+/**
+ * Clase SISISISISISIS cullo objetivo es decir isisisisis
+ * @author ignac
+ */
+public class SISISISISISIS {
+
+    /**
+     * @param args muy fachera la clase
+     */
+    public static void main(String[] args) {
+        /**
+         * @return devuelve sisisisisisisi
+         */
+        System.out.println("SISISISISIS");
+        
+        
+        int x=0;
+        int y=2;    //Variable
+        
+        /**
+         * @return 
+         */
+        
+        String jaja="Pito";
+        
+        
+        /**
+         * @override la variable toString es inutil
+         */
+    }
+    
+}
